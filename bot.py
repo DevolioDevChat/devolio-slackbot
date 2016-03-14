b1584b18ece1d200b52d5956a7fd563210438a71
g4s-slack <r.mhegyi@gmail.com>
2016-03-14 18:54:19 +0100

    minor bugfixes

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ slack = slacker.Slacker('xoxb-24649221783-q40uS6HJkH7D6TMhykeyaH7h')
 #     slack = slacker.Slacker(os.environ["SLACKAPIKEY"])
 #
 
+
 def open_im_channel(user):
     try:
         response = slack.im.open(user)
@@ -29,64 +30,11 @@ def open_im_channel(user):
     return response.body.get('channel', {}).get('id')
 
 
-async def read_loop(uri):
-    ws = await websockets.connect(uri)
-    while True:
-        # Wait for the data from slack to come in
-        json_data = await ws.recv()
-        data = json.loads(json_data)
-        print(data)
-
-        # If a user joins the devolio team
-        if data.get('type') == 'team_join':
-            # Get their user id and name
-            user_id = data.get('user').get('id')
-            user_name = data.get('user').get('name')
-            # Open im channel with user
-            im_channel_id = open_im_channel(user_id)
-            # Send intro message
-            if im_channel_id is not None:
-                sentences = ["Hey " + user_name + ", welcome to the Devolio Slack group!",
-                "We'd love to hear a little about you - feel free to drop" \
-                                            "in on <#intro> and let everyone know what you're about.",
-                                            "You can add your interests to your profile by clicking on your name, " \
-                                            "and then join channels for your various interests " \
-                                            "by clicking on that \"Channels\" link up near the top left."]
-                chat_message(sentences, user_id, .8)
-
-        # If a user changes their preferences
-        if data.get('type') == "user_change":
-            # Get their user id and name
-            user_id = data.get('user').get('id')
-            user_name = data.get('user').get('name')
-            # Make sure im channel is open
-            im_channel_id = open_im_channel(user_id)
-            user_title = data.get('user').get('profile').get('title')
-
-            if im_channel_id is not None:
-                scan_relevant_channels(user_id, user_title)
-
-        if data.get('type') == "message":
-            user_message = data.get('text')
-            channel_id = data.get('channel')
-            if user_message == "hi":
-                chat_message(["Beep boop, I'm a Welcome Bot!"], channel_id, 0)
-
 def chat_message(sentences, location_id, delay_time):
     for sentence in sentences:
         slack.chat.post_message(location_id, sentence)
         time.sleep(delay_time)
 
-def get_rtm_uri():
-    rtm = slack.rtm.start()
-    print(rtm)
-
-    try:
-        body = rtm.body
-    except slacker.Error as e:
-        print(e)
-        return None
-    return body.get('url')
 
 def scan_relevant_channels(user_id, user_title):
     shortcuts = {'web' : 'webdev', 'ror' : 'ruby', 'c++' : 'cplusplus',
@@ -101,6 +49,8 @@ def scan_relevant_channels(user_id, user_title):
     for title in user_title:
         if title in shortcuts and is_user_in_group(user_id, shortcuts[title]) == False:
             chat_message(["Hi, I noticed you've put " + shortcuts[title] + " in your profile. Why not join #" + shortcuts[title] + "?"], user_id, 0)
+
+
 def is_user_in_group(user_id, group_name):
     user_groups = slack.channels.list().body['channels']
     user_list = []
@@ -111,12 +61,67 @@ def is_user_in_group(user_id, group_name):
         return False
     return True
 
+
 def get_channel_names():
     user_groups = slack.channels.list().body['channels']
     channel_names = []
     for group in user_groups:
         channel_names.append(group['name'])
     return channel_names
+
+
+async def read_loop(uri):
+    ws = await websockets.connect(uri)
+    while True:
+        # Wait for the data from slack to come in
+        json_data = await ws.recv()
+        data = json.loads(json_data)
+        print(data)
+
+        # If a user joins the devolio team
+        if data.get('type') == 'team_join':
+            # Get their user id and name
+            user_id = data.get('user', {}).get('id')
+            user_name = data.get('user', {}).get('name')
+            # Open im channel with user
+            im_channel_id = open_im_channel(user_id)
+            # Send intro message
+            if im_channel_id is not None:
+                sentences = ["Hey " + user_name + ", welcome to the Devolio Slack group!",
+                             "We'd love to hear a little about you - feel free to drop"
+                             "in on <#intro> and let everyone know what you're about.",
+                             "You can add your interests to your profile by clicking on your name, "
+                             "and then join channels for your various interests "
+                             "by clicking on that \"Channels\" link up near the top left."]
+                chat_message(sentences, user_id, .8)
+
+        # If a user changes their preferences
+        if data.get('type') == "user_change":
+            # Get their user id
+            user_id = data.get('user', {}).get('id')
+            # Make sure im channel is open
+            im_channel_id = open_im_channel(user_id)
+            user_title = data.get('user', {}).get('profile', {}).get('title')
+
+            if im_channel_id is not None:
+                scan_relevant_channels(user_id, user_title)
+
+        if data.get('type') == "message":
+            user_message = data.get('text')
+            channel_id = data.get('channel')
+            if user_message == "hi":
+                chat_message(["Beep boop, I'm a Welcome Bot!"], channel_id, 0)
+
+
+def get_rtm_uri():
+    rtm = slack.rtm.start()
+
+    try:
+        body = rtm.body
+    except slacker.Error as e:
+        print(e)
+        return None
+    return body.get('url')
 
 # Check if this is the main application running (not imported)
 if __name__ == '__main__':

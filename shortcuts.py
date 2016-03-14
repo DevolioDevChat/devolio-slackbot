24c65d6f6e07ad18a1a969e643155a96c91c385a
Eden S.-G <edgro2693@gmail.com>
2016-03-14 18:31:23 -0400

    Fill out shortcuts

--- shortcuts.py
+++ shortcuts.py
@@ -1,6 +1,13 @@
 shortcuts = {
   'frontend': ['web', 'css', 'front-end', 'webdev', 'html', 'sass', 'scss', 'stylus'],
+  'wordpress': ['wp'],
+  'databases': ['sql', 'mysql', 'postgre', 'postgresql', 'db'],
   'ruby': ['rails', 'ror'],
   'cplusplus': ['c++', 'cpp'],
-  'javascript': ['js', 'jscript', 'ecmascript', 'es5', 'es6']
+  'javascript': ['js', 'jscript', 'ecmascript', 'es5', 'es6'],
+  'assembly': ['masm', 'nasm', 'tasm', 'asm'],
+  'c-sharp': ['c#'],
+  'go': ['golang'],
+  'linux': ['ubuntu', 'deb', 'debian', 'arch', 'gnu', 'opensuse'],
+  'sound': ['csound', 'max', 'supercollider', 'pd', 'max/msp', 'm4l']
 }
